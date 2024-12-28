38b28ec713f43524d88444569bb507a9ccee97b7
Bodi Ali <bodiali59@gmail.com>
2024-12-28 23:52:37 +0200

    Feat: Move controllers to authenticationController module

--- routes/authenticationRouter.js
+++ routes/authenticationRouter.js
@@ -1,32 +1,18 @@
 const { Router } = require("express");
-const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const db = require("../db/queries");
+const authenticationController = require("../controllers/authenticationController");
 
 const authenticationRouter = Router();
 
-authenticationRouter.get("/sign-up", (req, res) => {
-  res.render("sign-up-form");
-});
+authenticationRouter.get("/auth/google", authenticationController.authenticateWithGoogle);
 
-authenticationRouter.post(
-  "/sign-up",
-  asyncHandler(async (req, res) => {
-    const { firstName, lastName, username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await db.createUser(firstName, lastName, username, hashedPassword);
-    res.redirect("/");
-  })
-);
+authenticationRouter.get("/auth/google/callback", authenticationController.googleRedirect);
 
-authenticationRouter.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/",
-    failureFlash: true,
-  })
-);
+authenticationRouter.get("/sign-up", authenticationController.getSignUpForm);
+
+authenticationRouter.post("/sign-up", authenticationController.createUser);
+
+authenticationRouter.get("/log-in", authenticationController.getLogInForm);
+
+authenticationRouter.post("/log-in", authenticationController.authenticateWithLocal);
 
 module.exports = authenticationRouter;
