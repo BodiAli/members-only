847726a7bba94d419eda16cf9b669e67da7a2b9e
Bodi Ali <bodiali59@gmail.com>
2024-12-29 02:36:42 +0200

    Remove comment

--- app.js
+++ app.js
@@ -44,5 +44,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Express app listening on port: ${port}!`);
 });
-
-// TODO: Implement authentication using local and google auth strategies.
