47e8549ec1d9157e41b5f8c20c1115fabbdc430f
Bodi Ali <bodiali59@gmail.com>
2024-12-30 23:33:43 +0200

    Create queries to get posts and user specific posts

--- db/queries.js
+++ db/queries.js
@@ -13,3 +13,21 @@ exports.getUserByEmail = async (email) => {
   const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [email]);
   return rows;
 };
+
+exports.getAllPosts = async (limit, offset) => {
+  const { rows } = await pool.query(
+    "SELECT post_id, post_title, post_text, post_added, CONCAT(first_name, ' ', last_name) AS author FROM posts JOIN users ON post_user_id = user_id ORDER BY post_added DESC LIMIT $1 OFFSET $2;",
+    [limit, offset]
+  );
+  return rows;
+};
+
+exports.getUserPosts = async (userId) => {
+  const { rows } = await pool.query("SELECT * FROM posts WHERE post_user_id = $1", [userId]);
+  return rows;
+};
+
+exports.getPostCount = async () => {
+  const { rows } = await pool.query("SELECT COUNT(*) FROM posts;");
+  return Number.parseInt(rows[0].count, 10);
+};
