e4aa838fab2b5e4be563b2e10e943921db2a4b37
Bodi Ali <bodiali59@gmail.com>
2025-01-01 01:00:50 +0200

    Sanitize the page query parameter

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,22 +1,40 @@
 const asyncHandler = require("express-async-handler");
+const { query } = require("express-validator");
 const db = require("../db/queries");
 
-exports.getHomePage = asyncHandler(async (req, res) => {
-  const page = Number.parseInt(req.query.page, 10) || 1;
-  const limit = 5;
-  const offset = (page - 1) * limit;
+const validatePageQuery = [
+  query("page").customSanitizer(async (value) => {
+    if (value <= 0 || Number.isNaN(Number.parseInt(value, 10))) {
+      return 1;
+    }
 
-  const allPosts = await db.getAllPosts(limit, offset);
-  const totalPosts = await db.getPostCount();
-  const totalPages = Math.ceil(totalPosts / limit);
-  const memberShipMessage = req.flash("info");
+    const totalPosts = await db.getPostCount();
+    const limit = 5;
+    const totalPages = Math.ceil(totalPosts / limit);
+    if (value > totalPages) {
+      return totalPages;
+    }
+    return value;
+  }),
+];
 
-  console.log(req.user);
+exports.getHomePage = [
+  validatePageQuery,
+  asyncHandler(async (req, res) => {
+    const page = Number.parseInt(req.query.page, 10) || 1;
+    const limit = 5;
+    const offset = (page - 1) * limit;
 
-  res.render("index", {
-    allPosts,
-    page,
-    totalPages,
-    flashMessages: memberShipMessage.length > 0 ? memberShipMessage : null,
-  });
-});
+    const allPosts = await db.getAllPosts(limit, offset);
+    const totalPosts = await db.getPostCount();
+    const totalPages = Math.ceil(totalPosts / limit);
+    const memberShipMessage = req.flash("info");
+
+    res.render("index", {
+      allPosts,
+      page,
+      totalPages,
+      flashMessages: memberShipMessage.length > 0 ? memberShipMessage : null,
+    });
+  }),
+];
