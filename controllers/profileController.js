3c7e0b1281f4e5843992830398770378e5052e9f
Bodi Ali <bodiali59@gmail.com>
2024-12-31 19:39:24 +0200

    Rename to flashMessages for consistency

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -7,7 +7,7 @@ exports.getProfilePage = [
     const alreadySignedInMessage = req.flash("info");
 
     res.render("profile", {
-      alreadySignedInMessage: alreadySignedInMessage.length > 0 ? alreadySignedInMessage : null,
+      flashMessages: alreadySignedInMessage.length > 0 ? alreadySignedInMessage : null,
     });
   }),
 ];
