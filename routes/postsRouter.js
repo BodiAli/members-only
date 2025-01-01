9c76fe297d937ed6f33fb8760f3db8a3b3828f3a
Bodi Ali <bodiali59@gmail.com>
2025-01-01 22:42:55 +0200

    Handle request for /post/new route

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -3,6 +3,7 @@ const postsController = require("../controllers/postsController");
 
 const postsRouter = Router();
 
+postsRouter.get("/new", postsController.getCreatePostPage);
 postsRouter.post("/:id/delete", postsController.deletePost);
 
 module.exports = postsRouter;
