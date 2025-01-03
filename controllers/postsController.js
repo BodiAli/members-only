152906d19fe7e9ea859c4d191d6e597f1b9bf108
Bodi Ali <bodiali59@gmail.com>
2025-01-04 00:14:26 +0200

    Create middleware to creating a post && validation the form

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const authenticationController = require("./authenticationController");
 const db = require("../db/queries");
 
 exports.deletePost = [
@@ -11,6 +13,34 @@ exports.deletePost = [
   }),
 ];
 
-exports.getCreatePostPage = (req, res) => {
-  res.render("create-post");
-};
+exports.getCreatePostPage = [
+  authenticationController.isAuthenticated,
+  (req, res) => {
+    const postCreatedMessage = req.flash("info");
+
+    res.render("create-post", { flashMessages: postCreatedMessage.length > 0 ? postCreatedMessage : null });
+  },
+];
+
+const emptyErr = "can not be empty.";
+
+const validatePost = [
+  body("title").trim().notEmpty().withMessage(`Post title ${emptyErr}`),
+  body("postText").trim().notEmpty().withMessage(`Post text ${emptyErr}`),
+];
+
+exports.createPost = [
+  validatePost,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("create-post", { errors: errors.array() });
+    }
+    const { title, postText } = req.body;
+
+    await db.createPost(title, postText, req.user.user_id);
+
+    req.flash("info", "Post created successfully!");
+    return res.redirect("/post/new");
+  }),
+];
