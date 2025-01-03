28ff44ac4fe7a9c6cf4112431218e7d9f9e2b9e3
Bodi Ali <bodiali59@gmail.com>
2025-01-04 00:16:27 +0200

    Handle creating post form

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -4,6 +4,7 @@ const postsController = require("../controllers/postsController");
 const postsRouter = Router();
 
 postsRouter.get("/new", postsController.getCreatePostPage);
+postsRouter.post("/create", postsController.createPost);
 postsRouter.post("/:id/delete", postsController.deletePost);
 
 module.exports = postsRouter;
