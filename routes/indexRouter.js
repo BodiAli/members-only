1c2f20311358204f0f3bf05220aefd77dbb24da3
Bodi Ali <bodiali59@gmail.com>
2025-01-04 00:44:19 +0200

    Feat: Handle requests for /about route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,6 @@ const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getHomePage);
+indexRouter.get("/about", indexController.getAboutPage);
 
 module.exports = indexRouter;
